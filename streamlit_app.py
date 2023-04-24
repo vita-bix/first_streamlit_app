835355df572427eb860dea9ca1d00a61ff53a3a4
vita-bix <131662286+vita-bix@users.noreply.github.com>
2023-04-24 11:26:38 +0100

    Update streamlit_app.py
    creating variable fruits_selected
    pull rowsfrom full data set & assign to variable called fruits_to_show 
    use data in fruits_to_show in dataframe to display on page

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # pick list for customers to pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruits_list.loc[fruits_selected]
 
 # display table on page
 streamlit.dataframe(my_fruit_list)
