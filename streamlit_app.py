1f956044c22c625796b59fcd90390efaeaa01388
vita-bix <131662286+vita-bix@users.noreply.github.com>
2023-04-24 15:10:26 +0100

    Update streamlit_app.py
    adding header for advice 
    getting an api requests for fruits advice for watermelon

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display table on page
 streamlit.dataframe(fruits_to_show)
 
+
+# New section to display fruitvice api response 
+streamlit.header("Fruityvice Fruit Advice!")
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
