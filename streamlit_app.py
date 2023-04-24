676676f1cfe8ddcee832ffb796c9d5d84a4bc77a
vita-bix <131662286+vita-bix@users.noreply.github.com>
2023-04-24 11:20:52 +0100

    Update streamlit_app.py
    pre-populating to set example

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # pick list for customers to pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avacado','Strawberries'])
 
 # display table on page
 streamlit.dataframe(my_fruit_list)
