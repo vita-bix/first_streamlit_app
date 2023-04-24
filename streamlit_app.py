514c89c0271e127d401f49e1d930a2d73cd43fea
vita-bix <131662286+vita-bix@users.noreply.github.com>
2023-04-24 11:13:49 +0100

    Update streamlit_app.py
    adding a picklist

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,10 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# pick list for customers to pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# display table on page
 streamlit.dataframe(my_fruit_list)
+
