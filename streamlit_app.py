73a9594c52c6801da36336339228bb14b01f0b5f
vita-bix <131662286+vita-bix@users.noreply.github.com>
2023-04-24 11:05:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Specials Menu                 🥣 🥗 🐔 🥑 🍞               Breakfast Menu')
+streamlit.header('Specials                  🥣 🥗 🐔 🥑 🍞               Breakfast ')
 streamlit.text('🥗 Special English Breakfast                     🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Made with Love Eggs Benedict                  🥗 Kale,Spinach & Rocket Smoothie')
 streamlit.text('🥑 Health Freak Bowl                             🐔 Hard-Boiled Free-Range Egg')
