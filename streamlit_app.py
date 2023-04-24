100ec4b66827cecb54a3a08c1a0994897e7f92ee
vita-bix <131662286+vita-bix@users.noreply.github.com>
2023-04-24 11:27:15 +0100

    Update streamlit_app.py
    spelling error

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # pick list for customers to pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruits_list.loc[fruits_selected]
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display table on page
 streamlit.dataframe(my_fruit_list)
