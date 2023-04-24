26b4160b6c04047afcf9d8fc2e0d5488ff1a0785
vita-bix <131662286+vita-bix@users.noreply.github.com>
2023-04-24 11:28:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,5 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display table on page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
