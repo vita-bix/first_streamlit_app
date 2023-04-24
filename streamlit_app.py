5b56715e06bc6daa9c01698c66c97f2beab97859
vita-bix <131662286+vita-bix@users.noreply.github.com>
2023-04-24 11:02:04 +0100

    Update streamlit_app.py
    spacing

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Specials Menu                                Breakfast Menu')
-streamlit.text('Special English Breakfast                      Omega 3 & Blueberry Oatmeal')
-streamlit.text('Made with Love Eggs Benedict                   Kale,Spinach & Rocket Smoothie')
-streamlit.text('Health Freak Bowl                           Hard-Boiled Free-Range Egg')
+streamlit.header('Specials Menu                 ||               Breakfast Menu')
+streamlit.text('Special English Breakfast        ||              Omega 3 & Blueberry Oatmeal')
+streamlit.text('Made with Love Eggs Benedict      ||             Kale,Spinach & Rocket Smoothie')
+streamlit.text('Health Freak Bowl                    ||         Hard-Boiled Free-Range Egg')
 
 
