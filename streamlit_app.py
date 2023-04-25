e45556898706adb6eafaad4eee12da02d0ebde15
vita-bix <131662286+vita-bix@users.noreply.github.com>
2023-04-25 10:39:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ try:
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output the screen as a table
     streamlit.dataframe(fruityvice_normalized)
-  except URLError as e:
+ except URLError as e:
     streamlit.error()
 
 # don't run anything past here while we troubleshoot
